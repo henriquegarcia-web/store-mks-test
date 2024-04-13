1681a24b801625b2bc418d810e9a575c91cfbf56
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-04-13 09:38:28 -0300

    feat: Composição inicial do componente ProductList

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -5,11 +5,15 @@ import styles from './styles.module.scss'
 import { useGetProducts } from '@/hooks/getProducts'
 
 const ProductList = () => {
-  const { data: posts, error: postError, fetchStatus } = useGetProducts()
+  // const { data: posts, error: postError, fetchStatus } = useGetProducts()
 
-  console.log(posts)
+  // console.log(posts)
 
-  return <div className={styles.product_list}>Product List</div>
+  return (
+    <div className={styles.product_list}>
+      <div className={styles.product_list__wrapper}></div>
+    </div>
+  )
 }
 
 export default ProductList
