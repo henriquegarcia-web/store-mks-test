06c3609c4304d7996010f5940e259370e4954e5c
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-04-13 09:29:12 -0300

    feat: Composição inicial do componente MiniCart

--- src/components/MiniCart/index.tsx
+++ src/components/MiniCart/index.tsx
@@ -1,7 +1,21 @@
+import Image from 'next/image'
+
 import styles from './styles.module.scss'
 
 const MiniCart = () => {
-  return <div className={styles.minicart}>MiniCart</div>
+  const cartQuantity = 10
+
+  return (
+    <div className={styles.minicart}>
+      <Image
+        src="/minicart.svg"
+        alt="Ícone do Carrinho"
+        width={18}
+        height={18}
+      />
+      {cartQuantity && <p>{cartQuantity}</p>}
+    </div>
+  )
 }
 
 export default MiniCart
